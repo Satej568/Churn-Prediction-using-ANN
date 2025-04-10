424575170cde193a81a4e3b3c8360eeca335d26b
Satej568 <satejupatil.sp@gmail.com>
2025-04-10 11:42:08 +0530

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
-import numpy as np
-import tensorflow 
+import numpy as np 
 from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
 import pandas as pd
 import pickle
